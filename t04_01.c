a1abe8f2564d9e1f382d8deed9dfc755211a417a
AireneSitumorang <irensitumorang2020@gmail.com>
2025-02-27 16:28:11 +0700

    sudah

--- t04_01.c
+++ t04_01.c
@@ -2,32 +2,22 @@
 // 12S24058 - Airene Situmorang
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int _argv, char **_argc) {
-    char input[61]; 
-    printf("Input: ");
-    scanf("%s", input);
-
-    int length = strlen(input);
-    if (length < 3 || length > 60 || length % 3 != 0) {
-        printf("Input tidak valid.\n");
-        return 1;
+  char input[61];
+  scanf("%s", input);
+    
+    int lenght = strlen(input);
+    for (int i = 0; i < lenght; i+=3) {
+        char a[4] = {input[i], input[i+1], input[i+2], '\0'}; 
+        int ascii = atoi(a); 
+        printf("%c", ascii);  
     }
+    
+    printf("\n");  
 
-    printf("Output: ");
-    for (int i = 0; i < length; i += 3) {
-        char ascii_str[4];
-        ascii_str[0] = input[i];
-        ascii_str[1] = input[i+1];
-        ascii_str[2] = input[i+2];
-        ascii_str[3] = '\0';
-
-        int ascii_code = atoi(ascii_str);
-        printf("%c", (char)ascii_code);
-    }
-    printf("\n");
-   
-    return 0;
+  return 0;
 }
+ 
--- t04_02.c
+++ t04_02.c
@@ -5,21 +5,14 @@
 #include <string.h>
 
 int main(int _argv, char **_argc) {
-  char input[21]; 
-  printf("Input: ");
-  fgets(input, sizeof(input), stdin);
-
-  
-  size_t len = strlen(input);
-  if (len > 0 && input[len - 1] == '\n') {
-      input[len - 1] = '\0';
-  }
-
-  printf("Output: ");
-  for (int i = 0; i < strlen(input); i++) {
-      printf("%03d", (int)input[i]);
-  }
-  printf("\n");
-
-  return 0;
+  char string[21];
+    scanf("%20[^\n]", string);
+    int length = strlen(string);
+    for (int i = 0; i < length; i++) {
+        printf("%03d", string[i]);
+    }
+    printf("013");
+    return 0;
 }
+
+    
