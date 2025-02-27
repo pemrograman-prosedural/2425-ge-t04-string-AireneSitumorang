84f9c7bd3f077dfe39dfedcf22ee47d88608677e
AireneSitumorang <irensitumorang2020@gmail.com>
2025-02-27 16:16:37 +0700

    sudah diperbaiki

--- t04_01.c
+++ t04_01.c
@@ -1,10 +1,33 @@
-// NIM - Name
-// NIM - Name
+// 12S24053 - Dea Hutapea
+// 12S24058 - Airene Situmorang
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int _argv, char **_argc) {
-  // codes
+    char input[61]; 
+    printf("Input: ");
+    scanf("%s", input);
 
-  return 0;
+    int length = strlen(input);
+    if (length < 3 || length > 60 || length % 3 != 0) {
+        printf("Input tidak valid.\n");
+        return 1;
+    }
+
+    printf("Output: ");
+    for (int i = 0; i < length; i += 3) {
+        char ascii_str[4];
+        ascii_str[0] = input[i];
+        ascii_str[1] = input[i+1];
+        ascii_str[2] = input[i+2];
+        ascii_str[3] = '\0';
+
+        int ascii_code = atoi(ascii_str);
+        printf("%c", (char)ascii_code);
+    }
+    printf("\n");
+   
+    return 0;
 }
